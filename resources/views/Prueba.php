80861c0bf84876f2a69a33bf6b0dc399839b98c4
RDPA-GitHub <ronald.micros@gmail.com>
2023-06-26 20:59:57 -0400

    Laravel Proyect - 26/06/23 - Creacion de Routes y Views (Contacto, Blog, About, Prueba)

---
+++ resources/views/Prueba.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prueba - PHP</title>
+</head>
+<body>
+
+  <h1 style="color:blue; text-align: center; font-family: arial; margin-top: 35px;">
+      PRUEBA PHP
+  </h1>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Laravel-9.test
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Esta funcion me permite mostrar una fucion en especifico.
+/* Route::get('/', function () {
+    
+        return [
+            'Aprendible' => '.com',
+            'Laravel' => 'Projecto_1'
+        ];
+    
+    //return view('welcome');
+}); */
+
+// Para Mostrar una Vista normal podemos hacerlo de la siguiente manera
+Route::view('/', 'welcome')->name('home');
+
+// Generamos varias vistas a mostrar con distintas rutas
+
+    Route::view('/about', 'about')->name('about');
+    Route::view('/blog', 'blog')->name('blog');
+    Route::view('/contacto', 'contacto')->name('contacto');
+    Route::view('/Prueba', 'Prueba')->name('Prueba');
+    
